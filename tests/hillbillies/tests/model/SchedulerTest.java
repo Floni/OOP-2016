9f80dc1ad393e93b17e19a1c777f6fe3b51c07e9
Timothy Werquin <timothy@werquin.com>
2016-05-18 12:33:37 +0200

    Added empty task & scheduler tests

---
+++ tests/hillbillies/tests/model/SchedulerTest.java
@@ -0,0 +1,72 @@
+package hillbillies.tests.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by timo on 5/18/16.
+ */
+public class SchedulerTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void schedule() throws Exception {
+
+    }
+
+    @Test
+    public void isTaskAvailable() throws Exception {
+
+    }
+
+    @Test
+    public void getTask() throws Exception {
+
+    }
+
+    @Test
+    public void removeTask() throws Exception {
+
+    }
+
+    @Test
+    public void rebuildTask() throws Exception {
+
+    }
+
+    @Test
+    public void getAllTasksIterator() throws Exception {
+
+    }
+
+    @Test
+    public void getAllTasksStream() throws Exception {
+
+    }
+
+    @Test
+    public void getAllTasks() throws Exception {
+
+    }
+
+    @Test
+    public void containsAllTasks() throws Exception {
+
+    }
+
+    @Test
+    public void replace() throws Exception {
+
+    }
+
+    @Test
+    public void terminate() throws Exception {
+
+    }
+
+}
---
+++ tests/hillbillies/tests/model/TaskTest.java
@@ -0,0 +1,117 @@
+package hillbillies.tests.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by timo on 5/18/16.
+ */
+public class TaskTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void getSelectedPosition() throws Exception {
+
+    }
+
+    @Test
+    public void getName() throws Exception {
+
+    }
+
+    @Test
+    public void isWellFormed() throws Exception {
+
+    }
+
+    @Test
+    public void addScheduler() throws Exception {
+
+    }
+
+    @Test
+    public void removeScheduler() throws Exception {
+
+    }
+
+    @Test
+    public void getSchedulers() throws Exception {
+
+    }
+
+    @Test
+    public void isAssigned() throws Exception {
+
+    }
+
+    @Test
+    public void getAssignedUnit() throws Exception {
+
+    }
+
+    @Test
+    public void setAssignedUnit() throws Exception {
+
+    }
+
+    @Test
+    public void getPriority() throws Exception {
+
+    }
+
+    @Test
+    public void setPriority() throws Exception {
+
+    }
+
+    @Test
+    public void compareTo() throws Exception {
+
+    }
+
+    @Test
+    public void advanceTime() throws Exception {
+
+    }
+
+    @Test
+    public void isRunning() throws Exception {
+
+    }
+
+    @Test
+    public void await() throws Exception {
+
+    }
+
+    @Test
+    public void reset() throws Exception {
+
+    }
+
+    @Test
+    public void finish() throws Exception {
+
+    }
+
+    @Test
+    public void interrupt() throws Exception {
+
+    }
+
+    @Test
+    public void getVariable() throws Exception {
+
+    }
+
+    @Test
+    public void setVariable() throws Exception {
+
+    }
+
+}
