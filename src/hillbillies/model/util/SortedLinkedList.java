5c5e740ec6b74db30baa32d6cd138109eedde7d3
Timothy Werquin <timothy@werquin.com>
2016-05-20 21:33:42 +0200

    fix linkedList

--- src/hillbillies/model/util/SortedLinkedList.java
+++ src/hillbillies/model/util/SortedLinkedList.java
@@ -3,6 +3,7 @@ package hillbillies.model.util;
 import java.util.AbstractCollection;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 /**
  * A Linked List that is kept sorted.
@@ -59,19 +60,15 @@ public class SortedLinkedList<T extends Comparable<? super T>> extends AbstractC
      */
     @Override
     public boolean add(T n) {
-        if (backend.isEmpty())
-            backend.addFirst(n);
-        else if (n.compareTo(backend.getFirst()) <= 0)
-            backend.addFirst(n);
-        else if (n.compareTo(backend.getLast()) >= 0)
-            backend.addLast(n);
-        else {
-            int idx = 0;
-            while (idx < size() && n.compareTo(backend.get(idx)) > 0) {
-                idx++;
+        ListIterator<T> it = backend.listIterator();
+        while (it.hasNext()) {
+            T el = it.next();
+            if (n.compareTo(el) <= 0) {
+                it.previous();
+                break;
             }
-            backend.add(idx, n);
         }
+        it.add(n);
         return true;
     }
 
